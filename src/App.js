b506353f4a8badf42a56cccc52a081400416986e
jlum67 <90434758+jlum67@users.noreply.github.com>
2022-11-10 18:47:23 -0500

    prog

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 import { useState } from "react";
 import bakeryData from "./assets/bakery-data.json";
 
+import BakeryItem from "./components/BakeryItem";
+
 /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
 bakeryData.forEach((item) => {
   item.image = process.env.PUBLIC_URL + "/" + item.image;
@@ -12,16 +14,25 @@ function App() {
   // TODO: use useState to create a state variable to hold the state of the cart
   /* add your cart state code here */
 
+  const [total, setTotal] = useState(0)
+  const [cart, setCart] = useState([])
+
   return (
     <div className="App">
-      <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
+      <h1>Jeremy's Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
 
       {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
+        // <p>Bakery Item {index}</p> // replace with BakeryItem component
+        <BakeryItem name={item.name} description={item.description} price={item.price} image={item.image} total={total} cart={cart} setTotal={setTotal} setCart={setCart} />
       ))}
+
+      
 
       <div>
         <h2>Cart</h2>
+        <h3>{cart}</h3>
+        <h3>Total: {total}</h3>
+  
         {/* TODO: render a list of items in the cart */}
       </div>
     </div>
--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,41 @@
 // TODO: create a component that displays a single bakery item
+import "./BakeryItem.css";
+import { useState } from "react";
+
+
+export default function BakeryItem({name, price, description, image, total, cart, setTotal, setCart}) {
+
+    // const name = props.name;
+    // const description = props.description;
+    // const price = props.price;
+    // const image = props.image;
+
+
+	return (
+
+        <div class="border">
+            <h4>
+                <img src={image} width="500rem"/>
+            </h4>
+            <h4>
+                {name}
+            </h4>
+            <h5>
+                {description}
+            </h5>
+            <h5>
+                {price}
+            </h5>
+
+            <button onClick={() => {
+                    setTotal(total + price) 
+                    setCart([...cart, name])
+                }
+            }>Add to cart!</button>
+
+        </div>
+
+
+	);
+}
+
